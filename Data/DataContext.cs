2ae53c84928c0f7c56c4fa62cccaf126d7dbb378
Bryan Ramirez <dntern931@yahoo.com>
2025-01-27 20:19:49 -0600

    End of Section 2

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using System;
+using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+
+    
+}
