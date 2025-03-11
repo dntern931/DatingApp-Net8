5cd6db4eeba679e8ac6127cc7f046c4142658518
Bryan Ramirez <dntern931@yahoo.com>
2025-03-11 10:06:40 -0500

    End of Section 7

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -5,7 +5,7 @@ namespace DatingApp.Entities;
 public class AppUser
 {
     public int Id { get; set; }
-    public required string UserName { get; set; }
+    public required string Username { get; set; }
     public required byte[] PasswordHash { get; set; }
     public required byte[] PasswordSalt { get; set; }
 }
