5c6ddeccb80dc68227e68170f3f3cd762a62f734
Bryan Ramirez <dntern931@yahoo.com>
2025-03-29 18:21:58 -0500

    End of Section 10

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Claims;
 using AutoMapper;
 using DatingApp.Data;
 using DatingApp.DTOs;
@@ -11,7 +12,7 @@ using Microsoft.EntityFrameworkCore;
 namespace DatingApp.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers()
@@ -33,5 +34,23 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
         }
 
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if(username == null) return BadRequest("No username found in token");
+
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        if(user == null) return BadRequest("Could not find user");
+
+        mapper.Map(memberUpdateDto, user);
+
+        if(await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");
     }
 }
